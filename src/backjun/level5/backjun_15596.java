b74b9aef98682fcd9f483e221fa374ee3ab97efa
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-07-09 11:53:11 +0900

    함수 기초

---
+++ src/backjun/level5/backjun_15596.java
@@ -0,0 +1,11 @@
+package backjun.level5;
+
+public class backjun_15596 {
+    public static long sum(int[] a){
+        long answer = 0;
+        for (int i : a) {
+            answer += i;
+        }
+        return answer;
+    }
+}
