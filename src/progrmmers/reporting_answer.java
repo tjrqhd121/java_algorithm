c9282629a87f0131bed4d54aa5ec9e5d245ac0f3
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-07-16 20:38:14 +0900

    프로그래머스 신고 결과 받기 연습

---
+++ src/progrmmers/reporting_answer.java
@@ -0,0 +1,57 @@
+package progrmmers;
+import java.util.*;
+
+public class reporting_answer {
+    /*
+    프로그래머스 신고 결과 받기(시간 초과)
+     */
+    class Solution {
+        public int[] solution(String[] id_list, String[] report, int k) {
+            int[] answer = new int[id_list.length];
+            String reporter,reported;
+
+            //신고당한횟수
+            int[] reportedCnt = new int[id_list.length];
+
+            //report 중복제거
+            HashSet<String> hashSet = new HashSet<>(Arrays.asList(report));
+            String[] distinctReport = hashSet.toArray(new String[0]);
+
+            //초기값 설정
+            Arrays.fill(reportedCnt,0);
+
+            for(int i=0; i<distinctReport.length; i ++){
+                //신고자, 당한자
+                reporter = distinctReport[i].split(" ")[0];
+                reported = distinctReport[i].split(" ")[1];
+
+                for(int j=0, leng=id_list.length; j<leng; j++){
+                    if(reported.equals(id_list[j])){
+                        reportedCnt[j]++;
+                        break;
+                    }
+                }
+            }
+
+            //신고메일
+            for(int i=0, leng=reportedCnt.length; i<leng; i++){
+                if(k <= reportedCnt[i]){
+                    for(int j=0; j<distinctReport.length; j++){
+                        reporter = distinctReport[j].split(" ")[0];
+                        reported = distinctReport[j].split(" ")[1];
+                        if(reported.equals(id_list[i])){
+                            for(int t=0; t<id_list.length; t++){
+                                if(reporter.equals(id_list[t])){
+                                    answer[t]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+
+}
---
+++ src/progrmmers/reporting_answer_refactoring.java
@@ -0,0 +1,50 @@
+package progrmmers;
+import java.util.*;
+
+public class reporting_answer_refactoring {
+    /*
+    프로그래머스 신고 결과 받기 Map 활용(성공)
+     */
+    class Solution {
+        public int[] solution(String[] id_list, String[] report, int k) {
+            int[] answer = new int[id_list.length];
+            //   유저 , 신고한 사람 -> HashSet을 통한 중복 신고 제거
+            Map<String,HashSet<String>> reportMap = new HashMap<>();
+            // 신고자,신고성공메일카운트
+            Map<String,Integer> answerMap = new HashMap<>();
+
+            //초기화
+            for(int i=0; i<id_list.length; i++){
+                HashSet<String> setting = new HashSet<>();
+                reportMap.put(id_list[i],setting);
+                answerMap.put(id_list[i],0);
+            }
+
+            //누가 누구를 신고했는지? 에 대한 세팅
+            for(String s : report){
+                String[] tmp = s.split(" ");
+                String reporting = tmp[0];
+                String reported = tmp[1];
+                reportMap.get(reported).add(reporting);
+            }
+
+            //reportMap 을 통하여 신고메일 세팅
+            for(String userId : reportMap.keySet()){
+                HashSet<String> id = reportMap.get(userId);
+                if(id.size() >= k){
+                    for(String answerId : id){
+                        answerMap.put(answerId,answerMap.get(answerId)+1);
+                    }
+                }
+            }
+
+            //answerMap을 활용하여 정답 세팅
+            for(int i=0; i<id_list.length; i++){
+                answer[i] = answerMap.get(id_list[i]);
+            }
+
+            return answer;
+        }
+    }
+
+}
