ba5af56bf6a35482c1df2607783aac6f6abdd1cd
tjrqhd121 <tjrqhd121@naver.com>
2022-08-12 15:28:02 +0900

    백준 알고리즘

---
+++ src/backjun/level11/backjun25305.java
@@ -0,0 +1,24 @@
+package backjun.level11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class backjun25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(token.nextToken());
+        int k = Integer.parseInt(token.nextToken());
+        int[] array = new int[N];
+        token = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(token.nextToken());
+        }
+        Arrays.sort(array);
+        System.out.println(array[N - k]);
+    }
+}
