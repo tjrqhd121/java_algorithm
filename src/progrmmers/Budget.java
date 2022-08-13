042f6ace105f77e9d53a2316bca71bd3f8b97a15
tjrqhd121 <tjrqhd121@naver.com>
2022-08-13 23:07:47 +0900

    프로그래머스 1단계 : 예산.

---
+++ src/progrmmers/Budget.java
@@ -0,0 +1,33 @@
+package progrmmers;
+
+import java.util.Arrays;
+
+public class Budget {
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+
+        Arrays.sort(d);
+        for(int i = 0; i < d.length; i++){
+            sum += d[i];
+            if(sum > budget){
+                answer = i;
+                break;
+            }else if(sum == budget){
+                answer = i + 1;
+                break;
+            }else{
+                answer = d.length;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+            int[] d = {1,3,2,5,4};
+            int budget = 9;
+        System.out.println(solution(d,budget));
+    }
+}
+
